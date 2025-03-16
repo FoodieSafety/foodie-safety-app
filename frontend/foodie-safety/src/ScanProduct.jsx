726930d81b5cc4bdbc83075d42e1980bdea8b959
Temitope Emokpae <temokpae3@gmail.com>
2025-03-16 16:29:23 -0400

    updated the useState

--- frontend/foodie-safety/src/ScanProduct.jsx
+++ frontend/foodie-safety/src/ScanProduct.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import BarcodeScanner from './BarcodeScanner';
 import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const ScanProduct = ({ isLoggedIn, onLogout }) => {
-    
+
     return (
         <div>
             {/* Navbar */}
