af70793cdca0798e3441e9aba15775618bdab0d6
rpranavVT <rpranav@vt.edu>
2025-04-06 17:42:13 -0400

    Updated food fact api link to point to most recent iteration

--- backend/app/util/config.py
+++ backend/app/util/config.py
@@ -1,5 +1,5 @@
 # API variables
-OPENFOOD_API_URL = "https://world.openfoodfacts.net/api/v2/product/"
+OPENFOOD_API_URL = "https://world.openfoodfacts.org/api/v2/product/"
 
 # Local host variables
 FRONTEND_URL = "http://localhost:3000"
