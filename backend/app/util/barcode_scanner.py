3d460cda4a579ea1bb7bf23c3fd62f937328bd5c
Temitope Emokpae <temokpae3@gmail.com>
2025-05-05 12:16:39 -0400

    changed up the barcode_scanner.py

--- backend/app/util/barcode_scanner.py
+++ backend/app/util/barcode_scanner.py
@@ -7,9 +7,11 @@ import numpy as np
 from .schemas import ProductInfo, Barcode, ProductError
 from .config import OPENFOOD_API_URL
 from .dynamo_util import DynamoUtil
+from dotenv import load_dotenv
 
-# Get recall table name
-recall_table_name = os.getenv("DYNAMODB_RECALL_TABLE")
+# Load environment variables
+load_dotenv()
+
 
 def decode_image(product_img_bytes: bytes) -> List[Barcode]:
     np_img = np.frombuffer(product_img_bytes, np.uint8)
@@ -19,11 +21,14 @@ def decode_image(product_img_bytes: bytes) -> List[Barcode]:
     return barcodes
 
 def get_products_info(barcodes: List[Barcode], ddb_util: DynamoUtil) -> Tuple[List[ProductInfo], List[ProductError]]:
+    # Get recall table name
+    recall_table_name = os.getenv("DYNAMODB_RECALL_TABLE")
     product_info_list: List[ProductInfo] = []
     invalid_barcodes: List[ProductError] = []
     for barcode in barcodes:
         product_response = requests.get(f"{OPENFOOD_API_URL}{barcode.code}.json")
         if product_response.status_code == 200:
+            print(recall_table_name)
             product_json = product_response.json()
             product_recall = True if ddb_util.scan_table(recall_table_name, "UPCs", barcode.code) else False
             product_info_list.append(ProductInfo(code=barcode.code, name=product_json['product'].get('product_name'), brand=product_json['product'].get('brands'), recall=product_recall))
