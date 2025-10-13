958f6809c4dc49e1013cfe9adaa1735a5bee0d37
QingyueC <qingyuec@vt.edu>
2025-10-13 16:32:25 -0400

    reverse backend into basic version

--- backend/app/services/product_service.py
+++ backend/app/services/product_service.py
@@ -1,5 +1,4 @@
 import os
-from dotenv import load_dotenv
 from fastapi import APIRouter, status, Form, Depends
 from sqlalchemy.orm import Session
 from typing import List, Tuple
@@ -8,9 +7,6 @@ from ..controllers.product_controller import ProductController
 from ..util.database import get_db
 from ..util.oauth2 import get_current_user
 from ..util.dynamo_util import DynamoUtil
-
-# Ensure environment variables from .env are loaded for this module
-load_dotenv()
 
 # Create router object
 router = APIRouter(prefix="/products", tags=["Products"])
@@ -49,3 +45,19 @@ async def get_products(
     :return: response
     """
     return ProductController.get_products(db=db, token_data=token_data)
+
+@router.delete("", response_model=Tuple[List[ProductInfo], List[ProductError]], status_code=status.HTTP_202_ACCEPTED)
+async def delete_products(
+     str_barcodes: List[str] = Form(...),
+     db: Session = Depends(get_db),
+     token_data = Depends(get_current_user)
+):
+     """
+     Delete uploaded products
+     :param str_barcodes: list of barcode strings
+     :param db: session object
+     :param token_data: token
+     :return: response
+     """
+     barcodes = [Barcode(code=str_code) for str_code in str_barcodes]
+     return ProductController.delete_products(barcodes=barcodes, db=db, token_data=token_data)
