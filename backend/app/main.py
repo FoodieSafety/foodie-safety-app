3e613277b3fd5aec24206f44f5d0298b6601eac6
rpranavVT <rpranav@vt.edu>
2025-10-30 22:22:46 -0400

    Added the chat service to the backend router

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from middlewares.logging_middleware import log_requests
 from app.services.user_service import router as user_router
 from app.services.auth import router as auth_router
 from app.services.product_service import router as product_router
+from app.services.chat_service import router as chat_router
 from app.routes.subscription_routes import router as subscription_router
 from middlewares.cors_middleware import add_cors
 # Create a FastAPI instance
@@ -16,6 +17,7 @@ app = add_cors(app=app)
 app.include_router(user_router)
 app.include_router(auth_router)
 app.include_router(product_router)
+app.include_router(chat_router)
 app.include_router(subscription_router)
 
 @app.get("/")
