eaa7897af70db94341dc5c80a442ca176e973ef4
rpranavVT <rpranav@vt.edu>
2025-11-17 16:19:32 -0500

    Updated frontend api call and middleware restrictions to include current EC2 ip address

--- frontend/foodie-safety/src/config.js
+++ frontend/foodie-safety/src/config.js
@@ -2,7 +2,7 @@
 const config = {
   
   API_BASE_URL: process.env.NODE_ENV === 'production' 
-    ? 'http://foodiesafety.duckdns.org:8000' 
+    ? 'http://3.18.108.177:8000' //'http://foodiesafety.duckdns.org:8000' 
     : 'http://localhost:8000',
 };
 
