fa791a7e63f279a8af30419217a679d065db1548
rpranavVT <rpranav@vt.edu>
2025-12-11 18:59:58 -0500

    Updated cors to accept the base URL

--- backend/middlewares/cors_middleware.py
+++ backend/middlewares/cors_middleware.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.util.config import FRONTEND_URL
 from typing import List
 
-def add_cors(app: FastAPI, origins: List[str] = [FRONTEND_URL, "http://foodiesafety.duckdns.org:3000", "http://54.183.230.236:3000", "http://3.18.108.177:3000"]) -> FastAPI:
+def add_cors(app: FastAPI, origins: List[str] = [FRONTEND_URL, "http://foodiesafety.duckdns.org:3000", "http://foodiesafety.duckdns.org", "http://54.183.230.236:3000", "http://3.18.108.177:3000", "http://3.18.108.177"]) -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
